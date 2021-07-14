31edf50c33c6593abb33c33a9e147477ec611d6e
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-07-14 11:22:40 +0300

    Add files via upload

---
+++ project_5/js/script.js
@@ -0,0 +1,77 @@
+
+$('.sl').slick({
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+
+
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+
+
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+$('.sl').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    var current = $(slick.$slides[currentSlide]);
+    current.html(current.html());
+});
+
+function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("blogSlides");
+    var dots = document.getElementsByClassName("blogSlider__dots--dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+function remove(elem) {
+    elem.parentNode.removeChild(elem);
+}
+
